8fa5d2fd8535a259c8d89b3ebba646acfdf76da3
Tunahan Akhisar <tunaakhisar9@gmail.com>
2023-11-03 17:02:29 +0300

    #8 Account Controller Added, Login Page And Login View Model Added

---
+++ RunGroupWebApp/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Models;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginVm);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginVm.EmailAddress);
+            
+            if(user != null)
+            {
+                //User is found, check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user,loginVm.Password);
+
+                if (passwordCheck)
+                {
+                    //Password correct, sign in
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVm.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+                //Password is incorrect
+                TempData["Error"] = "Wrong credentials. Please try again";
+                return View(loginVm);
+            }
+            //User not found
+            TempData["Error"] = "Wrong credentials. Please try again";
+            return View(loginVm);
+        }
+    }
+}
